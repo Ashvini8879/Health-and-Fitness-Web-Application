4ead20e68a12dd2d39c0628cf210bfa55fe42f62
Ashvini8879 <ashvinidavale@gmail.com>
2024-08-05 20:10:50 +0530

    Delete Health and Fitness Web Application directory

--- Health and Fitness Web Application/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('explore-btn').addEventListener('click', function() {
-  document.querySelector('.explore-buttons').style.display = 'block';
-  document.querySelector('.video-container').style.transform = 'translateY(-50vh)';
-  document.querySelector('.video-container').style.overflowY = 'scroll';
-});
